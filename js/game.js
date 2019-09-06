b7ecd22baed409b9b549743268a8c0e72bb7b169
Bryant Franks <bryant.franks@gmail.com>
2019-09-05 23:13:22 -0400

    beginning of PRS

--- js/game.js
+++ js/game.js
@@ -1,3 +1,13 @@
-let computerPlay = () =>{
-
+const computerPlay = () =>{
+    let items = ['paper', 'rock', 'scissor']
+    let item = items[Math.floor(Math.random()*items.length)]
+    return item
 }
+
+function playRound(playerSelection, computerSelection){
+    // re-read switch section for this part
+}
+
+const playerSelection = 'rock'
+const computerSelection = computerPlay()
+console.log(playRound(playerSelection, computerSelection))
