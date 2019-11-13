4680ed795a75e9cd20d9aca263b3d47b40426d08
Bryant Franks <bryant.franks@gmail.com>
2019-11-12 23:39:08 -0500

    updated playRound

--- js/game.js
+++ js/game.js
@@ -1,13 +1,25 @@
 const computerPlay = () =>{
     let items = ['paper', 'rock', 'scissor']
     let item = items[Math.floor(Math.random()*items.length)]
+    console.log(`the computer chose ${item}`)
     return item
 }
+let playerSelection = 'scissor'
+const computerSelection = computerPlay()
 
 function playRound(playerSelection, computerSelection){
-    // re-read switch section for this part
-}
 
-const playerSelection = 'rock'
-const computerSelection = computerPlay()
+    // re-read switch section for this part
+    if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
+        return 'A TIE???  Try again!'
+    } else if (computerSelection.toLowerCase() =='rock' && playerSelection.toLowerCase() != 'paper') {
+        return 'You Lose! Try again!'
+    } else if (computerSelection.toLowerCase() == 'scissor' && playerSelection.toLowerCase() !='rock') {
+        return 'You Lose! Try again!'
+    } else if (computerSelection.toLowerCase() == 'paper' && playerSelection.toLowerCase() !='scissor') {
+        return 'You Lose! Try again!'
+    } else {
+        return 'YOU WIN!'
+    }
+}
 console.log(playRound(playerSelection, computerSelection))
